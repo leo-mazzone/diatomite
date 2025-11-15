29efaf885cb374434e51d003a17b1cc6cfe9f126
Leonardo Mazzone <leo@mazzone.space>
2025-11-15 14:17:57 +0000

    feat: implement connected components via disjoint sets

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,16 @@
 use sha2::{Digest, Sha256};
+use std::collections::HashMap;
 
 fn hash_bytes(b: &[u8]) -> [u8; 32] {
     let mut hasher = Sha256::new();
     hasher.update(b);
     hasher.finalize().into()
+}
+
+pub struct Merge {
+    pub left_key: String,
+    pub right_key: String,
+    pub probability: f32,
 }
 
 pub struct Cluster {
@@ -23,6 +30,57 @@ impl Cluster {
         }
     }
 }
+
+struct DisjointSet {
+    parent: Vec<usize>,
+    rank: Vec<u32>,
+}
+
+impl DisjointSet {
+    fn new(size: usize) -> Self {
+        DisjointSet {
+            parent: (0..size).collect(),
+            rank: vec![0; size],
+        }
+    }
+
+    fn find(&mut self, x: usize) -> usize {
+        if self.parent[x] != x {
+            self.parent[x] = self.find(self.parent[x])
+        }
+        self.parent[x]
+    }
+
+    fn link(&mut self, x: usize, y: usize) {
+        if self.rank[x] > self.rank[y] {
+            self.parent[y] = x;
+        } else {
+            self.parent[x] = y;
+            if self.rank[x] == self.rank[y] {
+                self.rank[y] += 1;
+            }
+        }
+    }
+
+    fn union(&mut self, x: usize, y: usize) {
+        let x_root = self.find(x);
+        let y_root = self.find(y);
+        self.link(x_root, y_root);
+    }
+
+    fn get_components(&mut self) -> Vec<Vec<usize>> {
+        let mut components: HashMap<usize, Vec<usize>> = HashMap::new();
+        for x in 0..self.parent.len() {
+            let root = self.find(x);
+            components.entry(root).or_insert(Vec::new()).push(x);
+        }
+        components.into_values().collect()
+    }
+}
+
+// fn merges_to_clusters(merges: Vec<Merge>) -> Vec<Cluster> {
+//     ...
+// }
 
 #[cfg(test)]
 mod tests {
@@ -45,5 +103,12 @@ mod tests {
 
         let combined = Cluster::combine(3, vec![c1, c2], 0.75);
         println!("{}", general_purpose::STANDARD.encode(combined.hash));
+    }
+
+    #[test]
+    fn connected_components() {
+        let mut djs = DisjointSet::new(100);
+        djs.union(0, 1);
+        assert_eq!(djs.get_components().len(), 99);
     }
 }
